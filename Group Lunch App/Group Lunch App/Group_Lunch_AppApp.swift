280543d0bfa2cc68b7ae80a6345c2de270e5327e
Jonathan Zhao <jonathanzhaon@gmail.com>
2022-04-09 17:20:09 -0400

    Created Xcode Project

---
+++ Group Lunch App/Group Lunch App/Group_Lunch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group_Lunch_AppApp.swift
+//  Group Lunch App
+//
+//  Created by Jonathan Zhao on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Group_Lunch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
